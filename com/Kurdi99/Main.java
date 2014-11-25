8e8aca71566a630372b3b3d2c097d4e87d7eef21
Kurdi99 <hamza.alkurdi@ics.edu.jo>
2014-11-25 09:34:44 +0200

    Create Main.java

---
+++ com/Kurdi99/Main.java
@@ -0,0 +1,14 @@
+package com.Kurdi99;
+import org.bukkit.plugin.java.javaplugin
+
+
+public class Main extends JavaPlugin {
+
+public void onEnable() {
+//Later On
+}
+public void onDisable() {
+//Later On
+}
+
+}
